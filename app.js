fb8cc82bf7ddea45ed3ab15df9e5940e4f9af5cc
parteek <parteekjain90340@gmail.com>
2021-01-11 19:46:54 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+// require('dotenv').config();
+
+// const express = require('express');
+// const bodyparser = require('body-parser');
+// const mongoose = require('mongoose');
+
+// const app = express();
+
+// app.use(bodyparser.json());
+// app.use(bodyparser.urlencoded({ extended: true }));
+
+// mongoose.Promise = global.Promise;
+
+// mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }).then(() => { console.log("DB is connected"); }
+// ).catch((err) => {
+//     console.log(err);
+//     process.exit();
+// })
+
+// app.get('/', (req, res) => {
+//     res.send("Welcome to the class");
+// });
+// // app.get('/check',(req,res)=>{
+// //     res.send("checking");
+// // })
+
+// require('./routes/route')(app);
+
+// const Port = process.env.PORT || 3000;
+
+// app.listen(Port, (err) => {
+//     if (err) console.log(err);
+//     else console.log(`port is live at ${Port}`);
+// });
+
+require('dotenv').config();
+
+const express = require("express");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }).then(() => {
+    console.log("DB is connected")
+}).catch((err) => {
+    console.log(err);
+    process.exit();
+});
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the class");
+});
+
+
+require('./routes/route')(app);
+
+const Port = process.env.PORT || 3000;
+app.listen(Port, (err) => {
+    if (err) console.log(err);
+    else console.log(`port is live at ${Port}`);
+})
+
---
+++ controllers/controllers.js
@@ -0,0 +1,81 @@
+const blog = require('../model/model');
+
+exports.getall = (req, res) => {
+    blog.find()
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch((err) => {
+            if (err) res.status(500).json(err);
+        });
+}
+
+exports.getone = (req, res) => {
+    blog.findById(req.params.blogID)
+        .then((data) => {
+            if (!data) return res.status(404).json({ "msg": "Blog not found" });
+            res.status(200).json(data);
+        })
+        .catch((err) => {
+            if (err) res.status(500).json(err);
+        });
+}
+
+exports.create = (req, res) => {
+    const newblog = new blog({
+        title: req.body.title,
+        author: req.body.author,
+        desc: req.body.desc
+    });
+
+    newblog.save()
+        .then((blog) => {
+            res.status(201).json({ "msg": "created", "blog": blog });
+        })
+        .catch((err) => {
+            if (err) return res.status(500).json(err);
+        })
+}
+
+exports.updateone = (req, res) => {
+
+    if (!req.body.title || !req.body.desc || !req.body.author)
+        return res.status(500).json({ "msg": "fill all the fields" });
+
+    Blog.findByIdAndUpdate(req.params.blogID, {
+        title: req.body.title,
+        author: req.body.author,
+        desc: req.body.desc
+    }, { new: true })
+        .then((data) => {
+
+            if (!data) return res.status(404).json({ "msg": "Not found" });
+            res.status(202).json({
+                "msg": "updated",
+                "doc": data
+            });
+        })
+        .catch((err) => {
+            if (err) res.status(500).json(err)
+        })
+}
+
+exports.deleteone = (req, res) => {
+
+    Blog.findByIdAndDelete(req.params.blogID)
+        .then((data) => {
+
+            if (!data) return res.status(404).json({ "msg": "Blog not found" });
+
+            res.status(202).json({
+                "msg": "deleted",
+                "doc": data
+            });
+
+        })
+        .catch((err) => {
+            if (err) res.status(500).json(err);
+        });
+
+}
+
